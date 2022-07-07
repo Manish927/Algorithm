6e3a73a1ae6ecf19640553676778b960ee7312ee
Manish Srivastava <mannu927@yahoo.com>
2022-07-07 11:39:46 +0530

    Update LC767.cpp
    solution

--- LC767.cpp
+++ LC767.cpp
@@ -22,3 +22,58 @@ Constraints:
 1 <= s.length <= 500
 s consists of lowercase English letters.
 */
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        typedef std::pair<char, int> PairT;
+        auto cmp = [&](PairT a, PairT b) { return a.second < b.second; };
+        typedef std::unordered_map<char, int> FreqMapT;
+        FreqMapT freq_map;
+        FreqMapT::iterator freq_iter;
+        
+        for (auto elem : s) {
+            freq_iter = freq_map.find(elem);
+            
+            if (freq_iter != freq_map.end()) {
+                freq_iter->second++;
+            }
+            else
+                freq_map.insert({elem, 0});
+        }
+        
+        std::string result;
+        std::priority_queue<PairT, std::vector<PairT>, decltype(cmp)> maxHeap{cmp};
+        for (auto [first, second] : freq_map) {
+            maxHeap.push({first, second});
+        }
+        
+        
+        auto start = maxHeap.top(); maxHeap.pop();
+        result.push_back(start.first);
+        start.second--;
+        
+        while (!maxHeap.empty()) {
+            auto temp = maxHeap.top(); maxHeap.pop();
+            result.push_back(temp.first);
+            temp.second--;
+            
+            if (start.second >= 0) {
+                maxHeap.push(start);
+            }
+            
+            start = temp;
+        }
+        
+        if (start.second >= 0) 
+            return "";
+        
+        return result;
+    }
+    
+
+};
