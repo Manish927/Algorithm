eed0f40cacd38da0bb1dcea69b5a067d43465dba
Manish Srivastava <mannu927@yahoo.com>
2025-09-19 12:05:55 +0530

    Create LC0.cpp

---
+++ LC0.cpp
@@ -0,0 +1,14 @@
+/*
+   Write a function that takes in a list of unique strings and returns a list of semordnilap pairs. A semordnilap pair is defined as a set of different
+   strings where the reverse of one word is the same as the forward version of the other. For example, the words "diaper" and "repaid" are a semordnilap pair,
+   as the words "palindromes" and "semordnilap".
+
+   The order of the returned pairs and the order of the string within each pair do not matter.
+
+   Sample input:
+   words = ["diaper", "abc", "test", "cba", "repaid"]
+   sample output
+   [["diaper, "repaid"], ["abc", "cba"]]
+
+   
+*/
