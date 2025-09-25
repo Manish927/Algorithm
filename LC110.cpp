3242dd24010719b0a82d806407bcb9f09d64d512
Manish Srivastava <mannu927@yahoo.com>
2025-09-25 09:13:21 +0000

    recursive way

--- LC110.cpp
+++ LC110.cpp
@@ -1,4 +1,30 @@
 /*
 110. Balanced Binary Tree
 Given a binary tree, determine if it is height-balanced
+
+Input: root = [3,9,20,null,null,15,7]
+Output: true
+
 */
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return checkHeight(root) != -1;
+    }
+
+private:
+    int checkHeight(TreeNode* node) {
+        if (!node) return 0;
+
+        int leftHeight = checkHeight(node->left);
+        if (leftHeight == -1) return -1;
+
+        int rightHeight = checkHeight(node->right);
+        if (rightHeight == -1) return -1;
+
+        if (abs(leftHeight - rightHeight) > 1) return -1;
+
+        return max(leftHeight, rightHeight) + 1;
+    }
+};
