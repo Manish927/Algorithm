d1928da134cdb6dfada0ac455e90ae7311fdf279
Manish Srivastava <mannu927@yahoo.com>
2025-10-01 11:48:28 +0000

    Using BFS + tracking

--- LC863.cpp
+++ LC863.cpp
@@ -18,5 +18,65 @@ public:
     }
 
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        std::unordered_map<TreeNode*, TreeNode*> parentsMap;
+        std::queue<TreeNode*> Q;
+        Q.push(root);
+        std::unordered_map<TreeNode*, int> seen;
+        int distance = 0;
+
+        //Step1: Build the parents map using Breadth First Search
+        while (!Q.empty()) {
+			int sz = Q.size();
+			for (int i=0; i < sz; ++i)
+			{
+				TreeNode* current = Q.front();
+				Q.pop();
+
+				if (current->left != nullptr) {
+					Q.push(current->left);
+					parentsMap[current->left] = current;
+				}
+				if (current->right != nullptr) {
+					Q.push(current->right);
+					parentsMap[current->right] = current;
+				}
+			}
+        }
+
+        Q.push(target);
+        seen[target] = 1;
+
+        while (!Q.empty() && distance < k) {
+			int sz = Q.size();
+            distance++;
+            
+			for (int i = 0; i < sz; ++i) {
+				TreeNode* current = Q.front();
+				Q.pop();
+
+				if (current->left != nullptr && seen.find(current->left) == seen.end()) {
+					Q.push(current->left);
+					seen[current->left] = 1;
+				}
+				if (current->right != nullptr && seen.find(current->right) == seen.end()) {
+					Q.push(current->right);
+					seen[current->right] = 1;
+				}
+
+				if (parentsMap.find(current) != parentsMap.end() && seen.find(parentsMap[current]) == seen.end()) {
+					Q.push(parentsMap[current]);
+					seen[parentsMap[current]] = 1;
+				}
+			}
+        }
+
+        std::vector<int> result;
+        while(!Q.empty()) {
+            TreeNode* node = Q.front();
+            Q.pop();
+            result.push_back(node->val);
+        }
+
+        return result;
     }
 }
