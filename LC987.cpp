55b9c0b45689b44904346b6d709189a49bd6bfd5
Manish Srivastava <mannu927@yahoo.com>
2025-10-02 07:27:50 +0000

    Initialization

--- LC987.cpp
+++ LC987.cpp
@@ -18,3 +18,32 @@ Column 1: Only node 20 is in this column.
 Column 2: Only node 7 is in this column.
 
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+
+ class Solution {
+public:
+    
+    Solution() {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+    }
+
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+    
+        std::vector<std::vector<int>> result;
+
+        return result;
+    }
+}
