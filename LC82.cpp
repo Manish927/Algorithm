f563c0e2110eb8d3200921ef48d60a96141f48f7
Manish Srivastava <mannu927@yahoo.com>
2025-10-03 15:49:52 +0000

    Linear solution of removing duplicat

--- LC82.cpp
+++ LC82.cpp
@@ -9,3 +9,70 @@ Input: head = [1,1,1,2,3]
 Output: [2,3]
 
 */
+
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode* prev = &dummy;
+
+        while (head && head->next) {
+            // Check if it's the start of duplicates
+            if (head->val == head->next->val) {
+                // Skip all nodes with current value
+                while (head->next && head->val == head->next->val) {
+                    head = head->next;
+                }
+                // Remove all duplicates
+                prev->next = head->next;
+            } else {
+                // Move prev if current node is unique
+                prev = prev->next;
+            }
+            head = head->next;
+        }
+        return dummy.next;
+    }
+};
+
+// Helper function to print linked list
+void printList(ListNode* node) {
+    while (node) {
+        cout << node->val << " ";
+        node = node->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Example: 1 -> 2 -> 3 -> 3 -> 4 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(3);
+    head->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next = new ListNode(4);
+    head->next->next->next->next->next->next = new ListNode(5);
+
+    cout << "Original list: ";
+    printList(head);
+
+    Solution sol;
+    ListNode* result = sol.deleteDuplicates(head);
+
+    cout << "List after removing duplicates: ";
+    printList(result);
+
+    return 0;
+}
