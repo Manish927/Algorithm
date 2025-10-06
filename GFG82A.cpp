6950f60568e6eff6097db619735d2a60e417756f
Manish Srivastava <mannu927@yahoo.com>
2025-10-06 05:58:44 +0000

    Linear solution

--- GFG82A.cpp
+++ GFG82A.cpp
@@ -11,3 +11,37 @@ Input: arr[] = [1, 2, 4]
 Output: [1, 2, 4]
 Explation:  As the array does not contain any duplicates so you should return [1, 2, 4].
 */
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> removeDuplicates(vector<int> &arr) {
+    std::vector<int> result;
+    int size = arr.size();
+    int idx = 1;
+    
+    if (arr.empty()) {
+        return result;
+    }
+    
+    result.push_back(arr[0]);
+    
+    for (int i = 1; i < size; i++) {
+        if (arr[i-1] != arr[i]) {
+            result.push_back(arr[i]);
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    vector<int> arr = {2, 2, 2, 2, 2};
+    vector<int> distinct = removeDuplicates(arr);
+    for (int num : distinct) {
+        cout << num << " ";
+    }
+    return 0;
+}
