cfb4af3f1efb1fe6130586ed2c069ae44565b639
Manish Srivastava <mannu927@yahoo.com>
2025-10-11 13:49:56 +0000

    Using Single Queue

--- LC225.cpp
+++ LC225.cpp
@@ -28,3 +28,35 @@ myStack.top(); // return 2
 myStack.pop(); // return 2
 myStack.empty(); // return False
 */
+
+//Using Single Queue - Push in O(n) and Pop() in O(1)
+
+class MyStack {
+private:
+    std::queue<int> q;
+
+public:
+    MyStack() {}
+
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int top = q.front();
+        q.pop();
+        return top;
+    }
+
+    int top() {
+        return q.front();
+    }
+
+    bool empty() {
+        return q.empty();
+    }
+};
