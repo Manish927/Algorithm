73d0f699590e894442f0a40a0e1593b926fce809
Manish Srivastava <mannu927@yahoo.com>
2026-07-26 15:04:14 +0530

    Add function to find frequent subtree sums
    Implement a solution to find the most frequent subtree sums in a binary tree.

--- LC508.cpp
+++ LC508.cpp
@@ -6,3 +6,53 @@ Given the root of a binary tree, return the most frequent subtree sum. If there 
 
 The subtree sum of a node is defined as the sum of all the node values formed by the subtree rooted at that node (including the node itself).
 */
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    private:
+    std::unordered_map<int, int> freq;
+    int maxFreq = 0;
+
+    int dfs(TreeNode* root) {
+        if (!root)
+            return 0;
+
+        int left = dfs(root->left);
+        int right = dfs(root->right);
+
+        int sum = left + right + root->val;
+
+        maxFreq = max(maxFreq, ++freq[sum]);
+
+        return sum;
+    }
+
+public:
+    vector<int> findFrequentTreeSum(TreeNode* root) {
+        if (!root)
+        return {};
+
+        dfs(root);
+
+        std::vector<int> ans;
+
+        for (auto &p : freq) {
+            if (p.second == maxFreq)
+                ans.push_back(p.first);
+        }
+
+        return ans;
+    }
+};
+
